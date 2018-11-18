972bc040033cbc8d4cb34bf67b5dd185e9a7ff09
Nolan Brady <nolanrbrady@gmail.com>
2018-11-18 19:01:20 +0100

    Format Data for Charts

--- Service/MetricServices/SalesSummary.js
+++ Service/MetricServices/SalesSummary.js
@@ -17,9 +17,30 @@ const formatTransactionsByDate = (transactions) => {
     },{})
 }
 
+const reduceByCurrentWeek = (weekDays, dailyTxs) => {
+    return weekDays.map(day => {
+        if (!dailyTxs[day]) return { x: DateTime.fromISO(day).weekdayShort, y: 0 }
+         return dailyTxs[day].reduce((accum, value) => {
+          return {
+            x: DateTime.fromISO(day).toLocal().weekdayShort,
+            y: accum.amount ? accum.amount : accum.y + value.amount
+          }
+        });
+      })
+}
+
+const generateWeekDays = () => {
+    return Array.from({ length: 7 }, (_, i) => {
+        let day = DateTime.local().minus({ days: i }).toISODate();
+        return day;
+      });
+}
+
 const generateSalesSummary = (transactions) => {
+    const weekDays = generateWeekDays();    
     const formatedTransactions = formatTransactionsByDate(transactions);
-    console.log("Formated Transactions", formatedTransactions);
+    const finalDate = reduceByCurrentWeek(weekDays, formatedTransactions)
+    console.log("Final Date", finalDate);
 }
 
 export default generateSalesSummary;
